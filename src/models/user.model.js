7ada1316cf525c1e4e2d4f71693212627701786a
Salman Khan <salmantechhub336@gmail.com>
2026-07-05 12:07:28 +0500

    changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,15 +1,30 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        require:[true, "Email is required"],
-        trim:true,
-        unique:true,
-        lowercase:true,
-        match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-            "Invalid Email Address"
-        ]
+  email: {
+    type: String,
+    require: [true, "Email is required"],
+    trim: true,
+    unique: [true, "email already exist"],
+    lowercase: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      "Invalid Email Address",
+    ],
+  },
+  name: {
+    type: String,
+    required: [true, "name is required for creating account"],
+  },
 
-    }
-})
+  password: {
+    type: String,
+    required: [true, "password is required for creating account"],
+    minlength:[6, "password should be atleast 6 character"],
+    select:false
+  },
+ 
+},
+ {
+    timestamps:true
+  });
