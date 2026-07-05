c3307c9e23d9c3247700b217f080583aced48f4f
Salman Khan <salmantechhub336@gmail.com>
2026-07-05 11:50:21 +0500

    create user Schema

---
+++ src/models/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        require:[true, "Email is required"],
+        trim:true,
+        unique:true,
+        lowercase:true,
+        match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/]
+
+    }
+})
