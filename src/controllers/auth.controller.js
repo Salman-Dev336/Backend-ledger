8aeeb9679fc9cb331be6fefb97303f3e178aab82
Salman Khan <salmantechhub336@gmail.com>
2026-08-24 16:19:28 +0500

    falied register of already existing email

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,23 @@
-const userModel = require("../models/user.model")
+const userModel = require("../models/user.model");
 
-// user register controller 
-//post /api/auth/register
+/**
+ * - user register controller
+ * - POST/api/auth/register
+ */
 
-function userRegisterController(req, res){
+async function userRegisterController(req, res) {
+  const { email, password, name } = req.body;
 
-    const {email, password, name} = req.body
-    
+  const isExists = await usermodel.findOne({
+    email: email,
+  });
+  if (isExists){
+    return res.status(422).json({
+        message: "user already register with this email",
+        status: "failed",
+
+    })
+  }
 }
 
-module.exports = { userRegisterController}
+module.exports = { userRegisterController };
