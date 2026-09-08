bb6c3d2d20172eda7ca88036cda030a2dac2114d
Salman Khan <salmantechhub336@gmail.com>
2026-09-09 00:07:11 +0500

    changes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,10 @@
 const express = require("express")
-const { userRegisterController } = require("../controllers/auth.controller")
+const authController = require("../controllers/auth.controller")
 
 const router = express.Router()
 
-
-router.post("/register", userRegisterController)
+/* POST /api/auth/Rehister */
+router.post("/register")
 
 
 
